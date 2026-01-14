86c9ccf2aed68c2f1f8ca3652910f49967726e29
Julien LEDENT <ledentj@gmail.com>
2026-01-14 13:23:51 +0100

    Add vérification token in middelware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,22 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export function middleware(request: NextRequest) {
+export async function middleware(req: NextRequest) {
   const response = NextResponse.next();
+  const { pathname } = req.nextUrl;
+
+  //Vérifier Role
+  if (pathname.startsWith("/admin")) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (!token) return NextResponse.redirect(new URL("/signIn", req.url));
+    if (token.role !== "ADMIN")
+      return NextResponse.redirect(new URL("/", req.url));
+  }
 
   // Vérifier si le guestId existe déjà
-  const existingGuestId = request.cookies.get("guestId");
+  const existingGuestId = req.cookies.get("guestId");
 
   if (!existingGuestId) {
     // Créer un nouveau guestId
