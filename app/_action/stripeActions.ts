cb496b3e322c374b64b0d2cc6bc9ed841c5f1693
Julien LEDENT <ledentj@gmail.com>
2026-01-19 13:17:47 +0100

    Resolve Stripe problem 3 - Mors logs

--- app/_action/stripeActions.ts
+++ app/_action/stripeActions.ts
@@ -17,6 +17,7 @@ export const createStripeSession = async (orderId: number) => {
         },
       },
     });
+    console.log("Le Order : " + order);
     const domain =
       process.env.NODE_ENV === "production"
         ? process.env.NEXT_PUBLIC_URL
@@ -42,6 +43,7 @@ export const createStripeSession = async (orderId: number) => {
       cancel_url: `${domain}/checkout`,
       customer_email: order?.shippingEmail,
     });
+    console.log("SessionURL : " + stripeSession.url);
     return { success: true, sessionUrl: stripeSession.url };
   } catch (error) {
     console.log(error);
