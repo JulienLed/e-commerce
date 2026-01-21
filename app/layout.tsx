26857af65981ef0dde5253a0fca784dc62c3b39a
Julien LEDENT <ledentj@gmail.com>
2026-01-21 13:56:36 +0100

    Begin Footer 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           <Toaster position="top-center" duration={3000} />
         </main>
 
-        <footer>
+        <footer className="sticky bottom-0 z-88">
           <Footer />
         </footer>
       </body>
--- components/server/footer/Footer.tsx
+++ components/server/footer/Footer.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div className="grid-cols-3">
-      <section id="links"></section>
-      <section id=""></section>
+    <div>
+      <div className="grid-cols-3">
+        <section id="about" className="flex flex-col gap-2">
+          <Image alt="logo" src={"/logo.png"} width={50} height={50} />
+          <span>{"Votre shop vape en ligne"}</span>
+        </section>
+        <section id="navigation">
+          <Link href={"/E-cigarette"}></Link>
+        </section>
+        <section id="legal"></section>
+      </div>
+      <section id="signature" className="w-sreen">
+        <p>© 2026 Smoke - Tous droits réservés</p>
+      </section>
     </div>
   );
 }
